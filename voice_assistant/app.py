8f6cc4c3d388ef249dc988026884be89b0c930b3
VarunKumar123456 <chitikenavarunkumar@gmail.com>
2026-05-24 15:34:40 +0530

    fixed runtime crash

--- voice_assistant/app.py
+++ voice_assistant/app.py
@@ -94,3 +94,7 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route('/')
+def home():
+    return "App is running"
